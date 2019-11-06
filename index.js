66dffd1766af5774aa3806cc6ac0441fa3afc8ad
hjrocha <hjrocha@gmail.com>
2019-11-06 11:32:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 exports.sayWhat = function() {
   console.log(“What?!”);
 }
+exports.sayWhatSingle = function() {
+  console.log('What?!');
+}
+exports.sayWhatBacktick = function() {
+  console.log(`What?!`);
+}
 
 console.log('teste')
 console.log('bump');
